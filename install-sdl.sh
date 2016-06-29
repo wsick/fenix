969ad8892dfb7de457a68a6b337e7d382ed521b9
Brad Sickles <brad_sickles@mckinsey.com>
2016-06-29 16:43:15 -0400

    perms

--- install-sdl.sh
+++ install-sdl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -n "$(which apt-get)" ]; then
-    apt-get install -y libsdl2{,-mixer,-image,-ttf}-dev
+    sudo apt-get install -y libsdl2{,-mixer,-image,-ttf}-dev
 elif [ -n "$(which yum)" ]; then
-    yum install SDL2{,_mixer,_image,_ttf}-devel
+    sudo yum install SDL2{,_mixer,_image,_ttf}-devel
 elif [ "$(uname)" -eq "Darwin" ]; then
-    brew install sdl2{,_image,_ttf,_mixer}
+    sudo brew install sdl2{,_image,_ttf,_mixer}
 else
     pacman -S mingw-w64-x86_64-gcc mingw-w64-x86_64-SDL2{,_mixer,_image,_ttf}
 fi
